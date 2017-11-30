a83d5b6a12ad4715aae2e96893af357d6dae7b1e
Rich A. Marino <ram06@acuitysso.com>
2017-11-30 00:41:26 -0500

    implemented a crude wave table, some cleanup

--- tonegen.c
+++ tonegen.c
@@ -8,6 +8,7 @@ LPCSTR AppName = "TesT";
 
 #define HARMONICS 12
 #define OCTAVES 8
+#define NUM_OUT_BUFFS 3
 //tcc build:
 // tiny_impdef C:\Windows\SysWOW64\winmm.dll
 // tcc32 tonegen.c -lwinmm
@@ -23,30 +24,36 @@ DWORD dwCallback = (DWORD) NULL;
 DWORD dwCallbackInstance = (DWORD) NULL;
 WAVEHDR pwh;
 MMRESULT mmres;
-unsigned char *buf=NULL;
+unsigned char *out_buf=NULL;
+//unsigned char **out_bufs = NULL;
+//int cur_buf = 0;
 unsigned char **bufs=NULL; //array of pointers to unsigned chars for holding note buffers.
+int *bufs_lengths = NULL; //array of ints to represent the length (in samples) of each buffer.
+int *bufs_cur = NULL;     //array to remember where the current index of the int.
 unsigned char enabled_mask[128]; //mask of signals that are enabled.
 
-#define SPS 22050
+#define SPS 44100 
 //44100
 #define Pi 3.1416
 
 //Clear the buffer (fill with 0s)
-void PurgeBuf()
+/*
+void PurgeBuf(int buf_id)
 {
+  printf("purgebuf: buf_id= %d", buf_id );
   for( int cnt = 0; cnt < SPS; ++cnt )
   {
-    buf[ cnt ] = 0;
+    //buf[ cnt ] = 127;
+    out_bufs[0][ cnt ] = 127;
   }
 }
-
-//Add a frequency to the buffer.
-void FillBufAdd(float freq,char amp,float phase)
+*/
+void PurgeBuf(int buf_id)
 {
-  register float x=freq*2*3.1416/SPS;
-  for(int cnt=0;cnt<SPS;cnt++)
+  printf("purgebuf: buf_id= %d", buf_id );
+  for( int cnt = 0; cnt < SPS; ++cnt )
   {
-    buf[cnt]+=(unsigned char)(cos(phase+cnt*x)*amp+127);
+    out_buf[ cnt ] = 127;
   }
 }
 
@@ -62,11 +69,15 @@ void FillBufAddB(unsigned char* int_buf, float freq,char amp,float phase)
 
 //attempt to replicate amiga harmonics program (it works but it's slow)
 //to the buffer specified in the first parameter.
-void FillBufAdd3B(unsigned char* int_buf, float freq, char amp, float phase)
+//void FillBufAdd3B(unsigned char* int_buf, float freq, char amp, float phase)
+//idx is the index of the buffer in the bufs array.
+void FillBufAdd3B(int idx, float freq, char amp, float phase)
 {
   //const int BASE_WAVEFORM_SIZE = SPS;
   //static double wave[BASE_WAVEFORM_SIZE];
   int i, j;
+  float period;
+  unsigned char* int_buf = bufs[idx];
   amp = amp*2;
   for( j = 1; j < OCTAVES; ++j )
   {
@@ -78,9 +89,19 @@ void FillBufAdd3B(unsigned char* int_buf, float freq, char amp, float phase)
       FillBufAddB( int_buf, freq / j, amp / ( 1 << j ), 0);
     }
   }
+  //the lowest frequency = freq / j. Get the period.
+  period = SPS / (freq / j);
+  printf("%d period: %d=%f\n", idx, (int)period, period);
+  bufs_lengths[idx] = (int)period;
+  if( ((float)period - (int)period) > 0.5 )
+  {
+    ++bufs_lengths[idx];
+    printf("%d fixed\n", bufs_lengths[idx]);
+  }
 }
 
 //attempt to replicate amiga harmonics program (it works but it's slow)
+/*
 void FillBufAdd3(float freq, char amp, float phase)
 {
   //const int BASE_WAVEFORM_SIZE = SPS;
@@ -98,16 +119,7 @@ void FillBufAdd3(float freq, char amp, float phase)
     }
   }
 }
-
-//Fill the buffer to generate a frequency.
-void FillBuf(float freq,char amp,float phase)
-{
-  register float x=freq*2*3.1416/SPS;
-  for(int cnt=0;cnt<SPS;cnt++)
-  {
-    buf[cnt]=(unsigned char)(cos(phase+cnt*x)*amp+127);
-  }
-}
+*/
 
 int GenFreq(void)
 {
@@ -127,9 +139,18 @@ int StopGen(void)
     return 1;//"Cant stop device.";
   return 0; 
 }
+
 int CloseDevice(void)
 {
-  free( buf );
+  int i;
+  /*
+  for( i = 0; i < NUM_OUT_BUFFS; ++i )
+  {
+    free( out_bufs[i] );
+  }
+  */
+  free( out_buf );
+
   mmres = waveOutReset( ghwo );
   if( mmres != MMSYSERR_NOERROR )
   {
@@ -144,6 +165,7 @@ int CloseDevice(void)
 }
 int PrepareDevice(void)
 {
+  int i;
   wfx.wFormatTag = WAVE_FORMAT_PCM;
   wfx.nChannels = 1;
   wfx.nSamplesPerSec = SPS;
@@ -155,20 +177,42 @@ int PrepareDevice(void)
   {
     return 1;//"Can't open device.";
   }
-  buf = (unsigned char *) calloc( SPS , 1);
+
+  /*
+  out_bufs = (unsigned char **) calloc( NUM_OUT_BUFFS, sizeof( unsigned char ** ) );
+  for(i = 0; i < NUM_OUT_BUFFS; ++i )
+  {
+    out_bufs[i] = (unsigned char *) calloc( SPS , 1);
+    if( out_bufs[i] == NULL )
+    {
+      CloseDevice();
+      printf("can't allocate memory");
+      return 1;
+    }
+  }
+  */
+  out_buf = (unsigned char *) calloc( SPS , 1);
+
+  printf("allocated!");
+  //buf = (unsigned char *) calloc( SPS , 1);
+  /*
   if( buf == NULL )
   {
     CloseDevice();
     printf("can't allocate memory");
     return 1;
   }
-  pwh.lpData = buf;
+  */
+  //pwh.lpData = out_bufs[i];
+  pwh.lpData = out_buf;
   pwh.dwBufferLength = SPS;
+  //pwh.dwFlags = 0;//WHDR_BEGINLOOP;
   pwh.dwFlags = WHDR_BEGINLOOP;
   pwh.dwLoops = 1;
   mmres = waveOutPrepareHeader( ghwo, &pwh, sizeof( pwh ) );
+  //pwh.dwFlags = WHDR_PREPARED;
   pwh.dwFlags = WHDR_BEGINLOOP | WHDR_ENDLOOP | WHDR_PREPARED;
-  pwh.dwLoops = 0xFFFFFFFF;
+  //pwh.dwLoops = 0xFFFFFFFF;
   if( mmres != MMSYSERR_NOERROR )
   {
     CloseDevice();
@@ -192,8 +236,14 @@ void UpdateNotes()
     {
       ++hamming_weight;
       for( j = 0; j < SPS; ++j )
+      //for( j = 0; j < bufs_lengths[i]; ++j )
       {
-        buf_t[j] += bufs[i][j];
+        ++bufs_cur[i];
+        if( bufs_cur[i] >= bufs_lengths[i])
+        {
+          bufs_cur[i] = 0;
+        }
+        buf_t[j] += bufs[i][ bufs_cur[i] ];
       }
       SendMessage( GetDlgItem( hWnd_main, 128 | i ), WM_SETTEXT, 0, "$");
     }
@@ -202,17 +252,20 @@ void UpdateNotes()
       SendMessage( GetDlgItem( hWnd_main, 128 | i ), WM_SETTEXT, 0, " ");
     }
   }
+
   //divide each sample by the number of signals that is enabled.
   if( hamming_weight == 0 )
   {
-    SecureZeroMemory( buf, SPS);
+    //SecureZeroMemory( out_bufs[ cur_buf ], SPS);
+    SecureZeroMemory( out_buf, SPS);
   }
   else
   {
     for( i = 0; i < SPS; ++i )
     {
       //printf(":%d\n", buf_t[i] );
-      buf[i] = buf_t[i] / hamming_weight;
+      //out_bufs[ cur_buf ][i]  = buf_t[i] / hamming_weight;
+      out_buf[i] = buf_t[i] / hamming_weight;
     }
   }
   free( buf_t );
@@ -226,7 +279,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     //const char* labels[] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "", "", "", ""};
     case WM_CREATE:
       { 
-      int i;
+      int i,j, first_value, second_value;
       const int key_width = 48;
       const int tall_key_height = 128;
       const int short_key_height = 72;
@@ -234,13 +287,17 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
       const int xbase = -1 * 46 * key_width + 10;
       hWnd_main = hWnd;
       bufs = (unsigned char **) calloc( 128, sizeof( unsigned char * ) );
+      bufs_lengths = (int*) calloc( 128, sizeof( int ) );
+      bufs_cur = (int*) calloc( 128, sizeof( int ) );
       printf("filling buffers...");
       for( i = 21; i < 88 + 21; ++i )
       { 
         int midi_note = i;
         float f = 440.0f * pow( 2.0f, ( (float)(midi_note - 69.0f) / 12.0f ) );
         bufs[i] = (unsigned char *) calloc( SPS, 1 );
-        FillBufAdd3B( bufs[i], f, 100, 0 );
+        //FillBufAdd3B( bufs[i], f, 100, 0 );
+        FillBufAdd3B( i, f, 100, 0 );
+
         if( i == 69 )
         {
           CreateWindowEx(0, "BUTTON", "A4", WS_CHILD | WS_VISIBLE, xbase + i * key_width, 24, key_width, tall_key_height, hWnd, (HMENU)( 128 + i ), NULL, NULL);
@@ -255,7 +312,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         {
           CreateWindowEx(0, "BUTTON", "", WS_CHILD | WS_VISIBLE, xbase + i * key_width, 24, key_width, tall_key_height, hWnd, (HMENU)( 128 + i ), NULL, NULL);
         }
-        printf("%d ", i );
+        //printf("%d ", i );
       }
 
       //setting enabled mask to 0.
@@ -263,9 +320,27 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
       {
         enabled_mask[i] = 0;
       }
+      printf("a");
       PrepareDevice();
-      PurgeBuf();
+      printf("b");
+      //PurgeBuf(cur_buf);
+      PurgeBuf(0);
+      printf("c");
       GenFreq();
+      printf("d");
+      /*
+      printf("\n\n");
+      for( i = 0; i < SPS; ++i )
+      {
+        printf("%d ", bufs[21][i]);
+      }
+    
+      printf("\n\n");
+      for( i = 0; i < SPS; ++i )
+      {
+        printf("%d ", bufs[108][i]);
+      }
+      */
       }//WM_CREATE
       break;
     case WM_DESTROY:
